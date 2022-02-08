14a0aac1dce4fdfe8de65b2a38e81905be128c37
sgaraba <sergiu.garaba@esempla.com>
2022-02-08 09:47:55 +0200

    First commit

---
+++ src/test/java/md/esempla/ti202202/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package md.esempla.ti202202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
